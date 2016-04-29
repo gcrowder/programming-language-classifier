73f628e8b51fff59a8c8d005b6213492e583d39d
gcrowder <gregory.j.crowder@gmail.com>
2016-04-29 09:21:38 -0400

    Classifier works. Kinda. Added tests.

--- classifier.py
+++ classifier.py
@@ -1,15 +1,19 @@
+import random
+import pandas as pd
 from sklearn.datasets import load_files
 from sklearn.cross_validation import train_test_split
 from sklearn.feature_extraction.text import HashingVectorizer
-from sklearn.naive_bayes import GaussianNB
+from sklearn.naive_bayes import MultinomialNB
 
 dataset = load_files('data', load_content=True,
                      encoding='UTF-8', decode_error='replace')
 
-docs_train, docs_test, y_train, y_test = train_test_split(
-    dataset.data, dataset.target, test_size=0.4, random_state=30)
+seed = random.randint(0, 10000)
 
-hv = HashingVectorizer()
+docs_train, docs_test, y_train, y_test = train_test_split(
+    dataset.data, dataset.target, test_size=0.4, random_state=seed)
+
+hv = HashingVectorizer(non_negative=True)
 
 train_code = hv.transform(docs_train)
 test_code = hv.transform(docs_test)
@@ -17,8 +21,8 @@ test_code = hv.transform(docs_test)
 # print("Train Row 2: {}.".format(train_code.getrow(2)))
 # print("Test Row 2: {}".format(test_code.getrow(2)))
 
-clf = GaussianNB
-clf.fit(train_code, hv.transform(y_train))
-
+clf = MultinomialNB()
+clf.fit(train_code, y_train)
+print("Seed: {}".format(seed))
 print("Train Score: {}".format(clf.score(train_code, y_train)))
 print("Test Score: {}".format(clf.score(test_code, y_test)))
