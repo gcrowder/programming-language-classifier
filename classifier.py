22949c55bc510a3031aea0cb742aac446f4e3384
gcrowder <gregory.j.crowder@gmail.com>
2016-04-30 11:24:38 -0400

    Add count vectorizer

--- classifier.py
+++ classifier.py
@@ -1,7 +1,7 @@
 import random
 from sklearn.datasets import load_files
 from sklearn.cross_validation import train_test_split
-from sklearn.feature_extraction.text import HashingVectorizer
+from sklearn.feature_extraction.text import HashingVectorizer, CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 
 dataset = load_files('data', load_content=True,
@@ -13,15 +13,24 @@ docs_train, docs_test, y_train, y_test = train_test_split(
     dataset.data, dataset.target, test_size=0.20, random_state=seed)
 
 hv = HashingVectorizer(non_negative=True)
+cv = CountVectorizer()
 
-train_code = hv.transform(docs_train)
-test_code = hv.transform(docs_test)
+hash_train_code = hv.transform(docs_train)
+hash_test_code = hv.transform(docs_test)
+
+count_train_code = cv.fit_transform(docs_train)
+count_test_code = cv.transform(docs_test)
 
 # print("Train Row 2: {}.".format(train_code.getrow(2)))
 # print("Test Row 2: {}".format(test_code.getrow(2)))
 
-clf = MultinomialNB(alpha=1.75)
-clf.fit(train_code, y_train)
+clf = MultinomialNB(alpha=1.50)
+clf.fit(hash_train_code, y_train)
 print("Seed: {}".format(seed))
-print("MultinomialNB Train Score: {}".format(clf.score(train_code, y_train)))
-print("MultinomialNB Test Score: {}".format(clf.score(test_code, y_test)))
+print("MultinomialNB Hash Train Score: {}".format(clf.score(hash_train_code, y_train)))
+print("MultinomialNB Hash Test Score: {}".format(clf.score(hash_test_code, y_test)))
+
+clf.fit(count_train_code, y_train)
+print("Seed: {}".format(seed))
+print("MultinomialNB Count Train Score: {}".format(clf.score(count_train_code, y_train)))
+print("MultinomialNB Count Test Score: {}".format(clf.score(count_test_code, y_test)))
