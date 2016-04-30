621eaa5cd79b3c18214d7d445bf9c6a59714428b
gcrowder <gregory.j.crowder@gmail.com>
2016-04-30 15:46:04 -0400

    Add function to print top 10 features for each target.

--- classifier.py
+++ classifier.py
@@ -1,8 +1,16 @@
 import random
+import numpy as np
 from sklearn.datasets import load_files
 from sklearn.cross_validation import train_test_split
 from sklearn.feature_extraction.text import HashingVectorizer, CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
+
+
+def show_top10(classifier, vectorizer, categories):
+    feature_names = np.asarray(vectorizer.get_feature_names())
+    for i, category in enumerate(categories):
+        top10 = np.argsort(classifier.coef_[i])[-10:]
+        print("%s: %s" % (category, " ".join(feature_names[top10])))
 
 dataset = load_files('data', load_content=True,
                      encoding='UTF-8', decode_error='replace')
@@ -27,10 +35,16 @@ count_test_code = cv.transform(docs_test)
 clf = MultinomialNB(alpha=1.50)
 clf.fit(hash_train_code, y_train)
 print("Seed: {}".format(seed))
-print("MultinomialNB Hash Train Score: {}".format(clf.score(hash_train_code, y_train)))
-print("MultinomialNB Hash Test Score: {}".format(clf.score(hash_test_code, y_test)))
+print("MultinomialNB Hash Train Score: {}".format(clf.score(hash_train_code,
+                                                            y_train)))
+print("MultinomialNB Hash Test Score: {}".format(clf.score(hash_test_code,
+                                                           y_test)))
 
 clf.fit(count_train_code, y_train)
 print("Seed: {}".format(seed))
-print("MultinomialNB Count Train Score: {}".format(clf.score(count_train_code, y_train)))
-print("MultinomialNB Count Test Score: {}".format(clf.score(count_test_code, y_test)))
+print("MultinomialNB Count Train Score: {}".format(clf.score(count_train_code,
+                                                             y_train)))
+print("MultinomialNB Count Test Score: {}".format(clf.score(count_test_code,
+                                                            y_test)))
+
+show_top10(clf, cv, dataset.target_names)
